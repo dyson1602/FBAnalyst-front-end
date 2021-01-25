eea32d5a5ed3f9bf87415b3248b677473c2f54c0
dyson1602 <cclark000@citymail.cuny.edu>
2021-01-24 21:32:19 -0500

    attempted styled components with charts. need to examine back-end of css framework to see how i can change it. built out the selector component for the radar chart, but may need to use a separate chart from another library that allows for greater adjustements

--- src/Components/playerRankerChart.js
+++ src/Components/playerRankerChart.js
@@ -6,6 +6,7 @@ import { DataTable } from 'primereact/datatable';
 import { Column } from 'primereact/column';
 import { Button } from 'primereact/button'
 import { combineValues } from '../StatFunctions/combineValues'
+import styled from 'styled-components'
 
 function PlayerRankerChart() {
 
@@ -15,12 +16,12 @@ function PlayerRankerChart() {
   const [combinedValues, setCombinedValues] = useState()
 
   useEffect(() => {
-    if(categories){
+    if (categories) {
       const fantasyValues = computeFantasyValue(playerAverages, categories)
       const combinedValues = combineValues(playerAverages, fantasyValues)
       setCombinedValues(combinedValues)
     }
-  },[categories])
+  }, [categories])
 
   const paginatorLeft = <Button type="button" icon="pi pi-refresh" className="p-button-text" />;
   const paginatorRight = <Button type="button" icon="pi pi-cloud" className="p-button-text" />;
@@ -29,7 +30,7 @@ function PlayerRankerChart() {
     <>
       <div>
         <div className="card">
-          <DataTable value={combinedValues} className="p-datatable-striped" paginator
+          <DataTable value={combinedValues} className="p-datatable" paginator
             paginatorTemplate="CurrentPageReport FirstPageLink PrevPageLink PageLinks NextPageLink LastPageLink RowsPerPageDropdown"
             currentPageReportTemplate="Showing {first} to {last} of {totalRecords}" rows={25} rowsPerPageOptions={[10, 20, 50]}
             paginatorLeft={paginatorLeft} paginatorRight={paginatorRight}>
@@ -48,7 +49,9 @@ function PlayerRankerChart() {
             <Column className="table-props" field="avg_blocks" header="Blk" sortable></Column>
             <Column className="table-props" field="avg_turnovers" header="TO" sortable></Column>
             <Column className="table-props" field="avg_points" header="Pts" sortable></Column>
-            <Column className="table-props" field="fNba_ftp" header="fFt%" sortable></Column>
+            {/* <ColoredColumn> */}
+              <ColoredColumn  field="fNba_ftp" header="fFt%" sortable></ColoredColumn>
+            {/* </ColoredColumn> */}
             <Column className="table-props" field="fNba_fgp" header="fFg%" sortable></Column>
             <Column className="table-props" field="fNba_tpm" header="fTPM" sortable></Column>
             <Column className="table-props" field="fNba_assists" header="fAst" sortable></Column>
@@ -72,3 +75,9 @@ function mdp(dispatch) {
 
 // export default connect(msp, mdp)(PlayerRankerChart)
 export default connect(null, mdp)(PlayerRankerChart)
+
+const ColoredColumn = styled(Column)`
+  background-color: "green"
+`
+
+// background: {${field > 0 ? "green" : field < 0 ? "pink" : "white"}}
--- src/Components/similarPlayerChart.js
+++ src/Components/similarPlayerChart.js
@@ -1,39 +1,97 @@
-import React from 'react'
+import React, { useState, useEffect } from 'react'
 import { Chart } from 'primereact/chart'
+import { useSelector } from 'react-redux'
+import { AutoComplete } from 'primereact/autocomplete'
 
-class SimilarPlayerChart extends React.Component {
 
-  state ={ 
-    player: 1
+const SimilarPlayerChart = () => {
+
+  const playerAverages = useSelector((state) => state.playerAverages)
+
+  const [firstPlayer, setFirstPlayer] = useState()
+  const [secondPlayer, setSecondPlayer] = useState()
+  const [filteredPlayers, setFilteredPlayers] = useState()
+
+  const searchPlayers = (event) => {
+    setTimeout(() => {
+      let filteredPlayers;
+      if (!event.query.trim().length) {
+        filteredPlayers = [...playerAverages];
+      }
+      else {
+        filteredPlayers = playerAverages.filter((allPlayers) => {
+          return allPlayers.name.toLowerCase().startsWith(event.query.toLowerCase());
+          // return allPlayers.name.toLowerCase().includes(event.query.toLowerCase());
+        });
+      }
+      setFilteredPlayers(filteredPlayers);
+    }, 250);
+    console.log(firstPlayer, secondPlayer)
   }
 
-  chartData = {
-    labels: ['Points', 'Rebounds', 'Assists', '3-Point', 'Blocks', 'Steals', 'FG %', "FT %"],
-    datasets: [
-      {
-        label: 'Lebron James',
-        backgroundColor: 'rgba(179,181,198,0.2)',
-        borderColor: 'rgba(179,181,198,1)',
-        pointBackgroundColor: 'rgba(179,181,198,1)',
-        pointBorderColor: '#fff',
-        pointHoverBackgroundColor: '#fff',
-        pointHoverBorderColor: 'rgba(179,181,198,1)',
-        data: [65, 59, 90, 81, 56, 55, 40]
-      },
-      {
-        label: 'Jerami Grant',
-        backgroundColor: 'rgba(255,99,132,0.2)',
-        borderColor: 'rgba(255,99,132,1)',
-        pointBackgroundColor: 'rgba(255,99,132,1)',
-        pointBorderColor: '#fff',
-        pointHoverBackgroundColor: '#fff',
-        pointHoverBorderColor: 'rgba(255,99,132,1)',
-        data: [28, 48, 40, 19, 96, 27, 100]
-      }
-    ]
-  };
+  const itemTemplate = (item) => {
+    return (
+      <div >
+        <div>{item.name}</div>
+      </div>
+    );
+  }
 
-  lightOptions = {
+  let chartAxes = {
+    labels: ['Points', 'Rebounds', 'Assists', '3-Point', 'Blocks', 'Steals', 'FG %', "FT %"]
+  }
+
+  let chartData;
+
+  function testChart() {
+    if (firstPlayer && secondPlayer) {
+      chartData = {
+        labels: ['Points', 'Rebounds', 'Assists', '3-Point', 'Blocks', 'Steals', 'FG %', "FT %"],
+        datasets: [
+          {
+            label: firstPlayer.name,
+            backgroundColor: 'rgba(179,181,198,0.2)',
+            borderColor: 'rgba(179,181,198,1)',
+            pointBackgroundColor: 'rgba(179,181,198,1)',
+            pointBorderColor: '#fff',
+            pointHoverBackgroundColor: '#fff',
+            pointHoverBorderColor: 'rgba(179,181,198,1)',
+            data: [
+              firstPlayer.avg_points,
+              firstPlayer.avg_tot_reb,
+              firstPlayer.avg_assists,
+              firstPlayer.avg_tpm,
+              firstPlayer.avg_blocks,
+              firstPlayer.avg_steals,
+              firstPlayer.fgp,
+              firstPlayer.ftp,
+              ]
+          },
+          {
+            label: secondPlayer.name,
+            backgroundColor: 'rgba(255,99,132,0.2)',
+            borderColor: 'rgba(255,99,132,1)',
+            pointBackgroundColor: 'rgba(255,99,132,1)',
+            pointBorderColor: '#fff',
+            pointHoverBackgroundColor: '#fff',
+            pointHoverBorderColor: 'rgba(255,99,132,1)',
+            data: [
+              secondPlayer.avg_points,
+              secondPlayer.avg_tot_reb,
+              secondPlayer.avg_assists,
+              secondPlayer.avg_tpm,
+              secondPlayer.avg_blocks,
+              secondPlayer.avg_steals,
+              secondPlayer.fgp,
+              secondPlayer.ftp,
+              ]
+          }
+        ]
+      };
+      return chartData
+    }
+  }
+  const lightOptions = {
     legend: {
       labels: {
         fontColor: '#495057'
@@ -48,13 +106,30 @@ class SimilarPlayerChart extends React.Component {
       }
     }
   };
-  render() {
-    return (
+
+
+  return (
+    <>
       <div className="card">
-        <Chart type="radar" data={this.chartData} options={this.lightOptions} />
+        <div style={{ display: "block", margin: "10px" }}>
+          <span className="p-fluid">
+            <h5>First Player</h5>
+            <AutoComplete value={firstPlayer} suggestions={filteredPlayers} completeMethod={searchPlayers} field="name" dropdown itemTemplate={itemTemplate} onChange={(e) => setFirstPlayer(e.value)} />
+            <h5>Second Player</h5>
+            <AutoComplete value={secondPlayer} suggestions={filteredPlayers} completeMethod={searchPlayers} field="name" dropdown itemTemplate={itemTemplate} onChange={(e) => setSecondPlayer(e.value)} />
+          </span>
+        </div>
+        {/* <Button onClick={clickHandler} label="Compare Trade" className="p-button-raised" style={{ margin: "10px" }} /> */}
       </div>
-    );
-  }
+      <div className="card">
+        {firstPlayer && secondPlayer ?
+          <Chart type="radar" data={testChart()} options={lightOptions} />
+          : <Chart type="radar" data={chartAxes} options={lightOptions} />
+        }
+      </div>
+    </>
+  );
+
 }
 
 
