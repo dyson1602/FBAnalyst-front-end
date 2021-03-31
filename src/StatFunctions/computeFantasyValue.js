c8a4bf1c91e84a64e753cfad248aa0c53008f703
dyson1602 <cclark000@citymail.cuny.edu>
2021-03-31 13:15:21 -0400

    removed: legacy code from computeFantasyValue

--- src/StatFunctions/computeFantasyValue.js
+++ src/StatFunctions/computeFantasyValue.js
@@ -64,36 +64,3 @@ function calculateValueModifier(statCategory) {
       return 1.0
   }
 }
-
- // for (const player in playerStatAverages) {
-    //   const currentPlayer = playerStatAverages[player]
-
-
-    //   const playerFantasyValues = {
-    //     name: currentPlayer.name,
-    //     nba_team_id: currentPlayer.nba_team_id,
-    //     position: currentPlayer.position,
-    //     fNba_mins: calculateStatFantasyValue(currentPlayer, leagueStatAverages, "avg_mins"),
-    //     fNba_fgm: calculateStatFantasyValue(currentPlayer, leagueStatAverages, "avg_fgm"),
-    //     fNba_fga: calculateStatFantasyValue(currentPlayer, leagueStatAverages, "avg_fga"),
-    //     fNba_fgp: calculateStatFantasyValue(currentPlayer, leagueStatAverages, "fgp"),
-    //     fNba_ftm: calculateStatFantasyValue(currentPlayer, leagueStatAverages, "avg_ftm"),
-    //     fNba_fta: calculateStatFantasyValue(currentPlayer, leagueStatAverages, "avg_fta"),
-    //     fNba_ftp: calculateStatFantasyValue(currentPlayer, leagueStatAverages, "ftp"),
-    //     fNba_tpm: calculateStatFantasyValue(currentPlayer, leagueStatAverages, "avg_tpm"),
-    //     fNba_tpa: calculateStatFantasyValue(currentPlayer, leagueStatAverages, "avg_tpa"),
-    //     fNba_tpp: calculateStatFantasyValue(currentPlayer, leagueStatAverages, "tpp"),
-    //     fNba_off_reb: calculateStatFantasyValue(currentPlayer, leagueStatAverages, "avg_off_reb"),
-    //     fNba_def_reb: calculateStatFantasyValue(currentPlayer, leagueStatAverages, "avg_def_reb"),
-    //     fNba_tot_reb: calculateStatFantasyValue(currentPlayer, leagueStatAverages, "avg_tot_reb"),
-    //     fNba_assists: calculateStatFantasyValue(currentPlayer, leagueStatAverages, "avg_assists"),
-    //     fNba_steals: calculateStatFantasyValue(currentPlayer, leagueStatAverages, "avg_steals"),
-    //     fNba_blocks: calculateStatFantasyValue(currentPlayer, leagueStatAverages, "avg_blocks"),
-    //     fNba_turnovers: calculateStatFantasyValue(currentPlayer, leagueStatAverages, "avg_turnovers"),
-    //     fNba_plus_minus: calculateStatFantasyValue(currentPlayer, leagueStatAverages, "avg_plus_minus"),
-    //     fNba_p_fouls: calculateStatFantasyValue(currentPlayer, leagueStatAverages, "avg_p_fouls"),
-    //     fNba_points: calculateStatFantasyValue(currentPlayer, leagueStatAverages, "avg_points"),
-    //   }
-    //   playerFantasyValues.fNba_score = fantasyValueAverage(playerFantasyValues, statCategory)
-    //   fantasyValuesArray.push(playerFantasyValues)
-    // }
